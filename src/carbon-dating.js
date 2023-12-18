b169ee30e736aeab87318e25be164466120572ca
ar97ar <an97ron@gmail.com>
2023-12-18 23:37:07 +0300

    feat: 4 task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,22 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity ) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (parseFloat(sampleActivity) && typeof sampleActivity === "string"){
+    num = parseFloat(sampleActivity)
+  } else {
+    return false
+  }
+
+  if (num > 0 && num <15){
+    let k = 0.693 / 5730
+    let res = (Math.log(15/num)) / k
+    return  Math.ceil(res)
+  } else {
+    return false
+  }
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam( members ) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let arr
+  if (Array.isArray(members)){
+    arr = members.filter(item => typeof item ==='string' && !parseFloat(item))
+  } else {
+    return false
+  }
+
+  if (arr.length === 0){
+    return false
+  }
+
+  let res = arr.map(item => item.trim().charAt(0).toUpperCase()).sort()
+
+  return res.join('')
 }
 
 module.exports = {
