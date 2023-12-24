781011cb950004bf5ac112b98f8e9bb0797913ec
ar97ar <an97ron@gmail.com>
2023-12-25 01:40:35 +0300

    feat: 4 task

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
+function encodeLine( str ) {
+  if (str === '') {
+    return ''
+  }
 
+  let arr = str.split('')
+  let cont = 1
+  let res = ''
+
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] == arr[i + 1]) {
+      cont++;
+    } else {
+      if (cont === 1) {
+        cont = ''
+      }
+      res += `${cont}${arr[i]}`;
+      cont = 1;
+    }
+  }
+  return res;
 }
+
 
 module.exports = {
   encodeLine
--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  const counts = {};
+  const res = names.map((name) => {
+    if (name in counts) {
+      let newName = `${name}(${ counts[name]++})`;
+      counts[newName] = 1;
+      return newName;
+    } else {
+      counts[name] = 1;
+      return name;
+    }
+  });
+
+  return res
 }
 
 module.exports = {
