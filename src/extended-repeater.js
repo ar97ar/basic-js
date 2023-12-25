9fb1bbc9f11d63e7788dfb2162d43f4c53d8c97a
ar97ar <an97ron@gmail.com>
2023-12-25 15:03:55 +0300

    feat: Extended repeater + Vigenere cipher

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let additions=''
+
+  if (!("separator" in options)){
+    options.separator='+'
+  }
+
+  if (!("additionSeparator" in options)){
+    options.additionSeparator='|'
+  }
+
+  if ("addition" in options){
+    additions=(options.addition+options.additionSeparator).repeat(options.additionRepeatTimes-1)+options.addition;
+  } else {
+    additions=options.additionSeparator.repeat(options.additionRepeatTimes-1)
+  }
+
+  return ((str+additions)+options.separator).repeat(options.repeatTimes-1)+(str+additions)
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,59 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, key) {
+    if (!str || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    return this.crypt(str, key, true)
+
   }
+  decrypt(str, key) {
+    if (!str || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    return this.crypt(str, key, false)
+  }
+
+  crypt(str, key, crypting) {
+    let shift = key.repeat(Math.ceil(str.length / key.length)).toUpperCase();
+    let upper_str = String(str).trim().toUpperCase();
+    let r = '';
+
+    for(let i = 0, j = 0; i < upper_str.length; i++) {
+      let code = upper_str.charCodeAt(i);
+
+      if(this.alphabet.indexOf(upper_str[i]) > -1) {
+        if(crypting) code = code + shift[j++].charCodeAt(0);
+        else code = (code + this.alphabet.length - shift[j++].charCodeAt(0));
+        code = code % this.alphabet.length + 65;
+      }
+
+      r += String.fromCharCode(code);
+
+    }
+
+    let res
+
+    if (this.direct) {
+      res = r
+    } else {
+      res = r.split('').reverse().join('')
+    }
+
+    return res
+  }
+
+
 }
+
 
 module.exports = {
   VigenereCipheringMachine
